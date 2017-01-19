88ee9570d335ddb1ed4d9ee8ecf6c714c17f87d8
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-01-19 17:59:35 -0300

    Added babot settings

---
+++ babot/slackbot_settings.py
@@ -0,0 +1,11 @@
+import os
+
+API_TOKEN = os.environ['BABOT_TOKEN']
+
+DEFAULT_REPLY = 'Can\'t understand you :('
+ERRORS_TO = 'babot-testing'
+
+PLUGINS = [
+    'slackbot.plugins',
+    'commands'
+]
