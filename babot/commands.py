ab3c08f3fbad8984faafaf4bf62446bfb1e5cc69
Ramon Saraiva <ramonsaraiva@gmail.com>
2017-01-20 01:41:38 -0300

    Split up messy code

--- babot/commands.py
+++ babot/commands.py
@@ -1,19 +1,24 @@
+from datetime import datetime
+
 from slackbot.bot import respond_to
 from slackbot.bot import listen_to
 
 import requests
 from bs4 import BeautifulSoup as BS
 
-@listen_to('babot subway')
-@respond_to('subway')
-def subway(message):
+def scrap_subway_lines():
+    '''
+    Scraps from enelsubte the current status for all
+    subway lines in Buenos Aires.
+    '''
     URL = 'http://enelsubte.com/estado/'
     request = requests.get(URL)
     markup = BS(request.text, 'html.parser')
 
-    status_table = markup.find('table', {'id': 'tabla-estado'})
     subway_status = []
-    updated_at = ''
+    updated_at = None
+
+    status_table = markup.find('table', {'id': 'tabla-estado'})
     for subway_line in status_table.findAll('tr'):
         try:
             name_markup, status_markup = subway_line.findAll('td')
@@ -21,15 +26,37 @@ def subway(message):
             status = status_markup.findAll('div')[0].text
             subway_status.append((name, status))
         except ValueError:
+            '''
+            This exception occurs in the last <tr> that does not have
+            two <td>'s to unpack, that means it is the update time row.
+            '''
             updated_at = subway_line.find('td').find('span')['title']
+    return subway_status, updated_at
 
-    output = 'Subway status for today!\n> '
+
+@listen_to('babot subway')
+@respond_to('subway')
+def subway(message):
+    '''
+    Shows the current status for all subway lines
+    in Buenos Aires
+    '''
+    subway_status, updated_at = scrap_subway_lines()
+
+    # Highlights non-normal subway status
     format_status = (lambda status: '`{}`'.format(status)
         if status != 'Normal' else status)
 
+    output = 'Subway status for now!\n> '
+
+    # All subway lines should be blockquoted for better reading
     output += '\n> '.join(
-        ['*{}* => {}'.format(sl[0], format_status(sl[1]))
-            for sl in subway_status])
+        ['*{}* => {}'.format(subway_line[0], format_status(subway_line[1]))
+            for subway_line in subway_status])
+
+    date_format = '%Y-%m-%dT%H:%M:%S-%f'
+    updated_at = datetime.strptime(updated_at, date_format)
+    output += '`\nUpdated at: {}'.format(updated_at.strftime("%d/%m %H:%M:%S"))
 
     message.react('metro')
     message.reply(output)
